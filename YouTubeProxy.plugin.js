d4b140437ceb0815b0bfb64c782ba97452fca6be
Nathan Franke <me@nathan.sh>
2022-08-29 22:29:46 -0500

    improve comments on script

--- YouTubeProxy.plugin.js
+++ YouTubeProxy.plugin.js
@@ -2,11 +2,16 @@
  * @name YouTubeProxy
  * @author nathanfranke
  * @description Changes YouTube embed links to a proxy, letting you play restricted videos easily.
- * @version 0.0.1
+ * @version 1.0.0
+ * @authorId 201537559740940288
+ * @donate https://paypal.me/natwf
+ * @source https://github.com/nathanfranke/discordytproxy
  */
 
 module.exports = class YouTubeProxy {
 	start() {
+		// Note for reviewers, this timer is hacky and inefficient, but it gets the job done.
+		// If you find a better solution, I will happily merge your contribution!
 		this.clock = setInterval(function () {
 			const re = /https:\/\/www.youtube.com\/embed\/([a-zA-Z0-9_-]{11})?/;
 			for (let frame of document.getElementsByTagName("iframe")) {
